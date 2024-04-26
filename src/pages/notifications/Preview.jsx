c68d39525298870b54b62a9173b0ce3aa73d09ed
hamzailyas237 <hamzailyas237@gmail.com>
2024-04-26 17:34:11 +0500

    vibe garden admin

---
+++ src/pages/notifications/Preview.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import Header from '../../content/webPreviewContent/Header'
+import emailTemplate from "../../assets/images/emailTemplate.png";
+const Preview = () => {
+  return (
+    <div>
+      {/* <Header /> */}
+      <img src={emailTemplate} alt="" />
+    </div>
+  );
+};
+
+export default Preview;
